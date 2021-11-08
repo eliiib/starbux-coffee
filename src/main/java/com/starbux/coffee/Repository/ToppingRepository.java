073e9e0743e6a37431bae34663e0792269f86701
elma <virgo.elham@gmail.com>
2021-11-08 20:54:50 +0330

    delete corrup files

--- src/main/java/com/starbux/coffee/Repository/ToppingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.starbux.coffee.Repository;
-
-import com.starbux.coffee.domain.Topping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToppingRepository extends JpaRepository<Topping, Long> {
-}
