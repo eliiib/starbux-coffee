a27679640571ad0d1b2fd8ddfce938e1a447b5d3
elma <virgo.elham@gmail.com>
2021-11-10 00:38:48 +0330

    Add topping integration test

--- src/main/java/com/starbux/coffee/controller/topping/CreateToppingResponse.java
+++ src/main/java/com/starbux/coffee/controller/topping/CreateToppingResponse.java
@@ -1,10 +1,14 @@
 package com.starbux.coffee.controller.topping;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateToppingResponse {
 
     private String name;
---
+++ src/test/java/com/starbux/coffee/controller/topping/ToppingResourceIT.java
@@ -0,0 +1,60 @@
+package com.starbux.coffee.controller.topping;
+
+
+import com.starbux.coffee.domain.Topping;
+import com.starbux.coffee.repository.ToppingRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ToppingResourceIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ToppingRepository toppingRepository;
+
+    @LocalServerPort
+    private int serverPort;
+
+    @BeforeEach
+    private void init() {
+        this.toppingRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("When the inputs are correct, a new topping should be added")
+    public void createTopping_validInput_toppingCreated() {
+        CreateToppingRequest request = new CreateToppingRequest();
+        request.setName("Milk");
+        request.setAmount(2D);
+        HttpEntity<CreateToppingRequest> httpEntity = new HttpEntity<>(request);
+        ResponseEntity<CreateToppingResponse> response = restTemplate.exchange(createUrl("/toppings"), HttpMethod.POST,
+                httpEntity, CreateToppingResponse.class);
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        assertThat(response.getBody().getName()).isEqualTo("Milk");
+        assertThat(response.getBody().getAmount()).isEqualTo(2D);
+
+        List<Topping> savedTopping = toppingRepository.findAll();
+        assertThat(savedTopping).isNotEmpty();
+        assertThat(savedTopping.get(0).getName()).isEqualTo("Milk");
+    }
+    private String createUrl(String address) {
+        return new StringBuilder("http://localhost:")
+                .append(serverPort).append("/").append(address).toString();
+    }
+
+}
